333deda5f30029caaadacbcd3d0ce6d2c8c7f801
Radulescu, Sorina (sr549q) <sr549q@att.com>
2019-06-24 23:00:34 +0300

    initial structure of the project

---
+++ firstAidHelper/src/main/java/com/ibm/fah/FirstAidHelperApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.fah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAidHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAidHelperApplication.class, args);
+	}
+
+}
---
+++ firstAidHelper/src/main/java/com/ibm/fah/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ibm.fah;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstAidHelperApplication.class);
+	}
+
+}
