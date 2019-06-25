4ee6a2c2cff7fa3d496fdcd2ed8378b0ef05567b
ana.mihalceanu@ro.ibm.com <ana.mihalceanu@ro.ibm.com@192.168.0.31>
2019-06-26 00:26:32 +0300

    deployment for dev namespace where mongo is configured within cluster

--- firstAidHelper/src/main/java/com/ibm/fah/mongodb/MongoConfig.java
+++ firstAidHelper/src/main/java/com/ibm/fah/mongodb/MongoConfig.java
@@ -1,22 +1,38 @@
 package com.ibm.fah.mongodb;
 
 import java.io.IOException;
-import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.*;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 public class MongoConfig {
-    private static final String MONGO_DB_URL = "localhost";
-    private static final String MONGO_DB_NAME = "embeded_db";
-    @Bean
-    public MongoTemplate mongoTemplate() throws IOException {
-        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-        mongo.setBindIp(MONGO_DB_URL);
-        MongoClient mongoClient = mongo.getObject();
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
-        return mongoTemplate;
-    }
+	@Value("${spring.data.mongodb.database}")
+	private String database;
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+	@Value("${spring.data.mongodb.port}")
+	private String port;
+	@Value("${spring.data.mongodb.username}")
+	private String username;
+	@Value("${spring.data.mongodb..password}")
+	private String password;
+
+	@Bean
+	public MongoTemplate mongoTemplate() throws IOException {
+		List<MongoCredential> allCred = new ArrayList<MongoCredential>();
+		System.out
+				.println("???????????????????" + username + " " + database + " " + password + " " + host + " " + port);
+		allCred.add(MongoCredential.createCredential(username, database, password.toCharArray()));
+		MongoClient client = new MongoClient(new ServerAddress(host, Integer.parseInt(port)), allCred);
+		return new MongoTemplate(client, database);
+	}
 }
